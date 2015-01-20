da3008c689969c42e5af5e6bbe645954493b58a8
Magnus Buk <Magnus.Buk@1-2-3.tv>
2015-01-20 10:40:33 +0100

    added tests folder

---
+++ tests/Bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ZF2XmlApiSendeffect;
+
+/**
+ * Bootstrap class for the unit tests
+ *
+ * @package ZF2XmlApiSendeffect
+ * @author Magnus Buk <MagnusBuk@gmx.de>
+ */
+class Bootstrap
+{
+    /**
+     * Init the unit tests
+     *
+     * @access public
+     * @return void
+     */
+    public static function init()
+    {
+
+    }
+}
+
+// Start bootstrapping
+Bootstrap::init();
